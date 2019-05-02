534be9aaddab97711987d256bb4179c3c81be080
Nikolas Freitas <nikolas.freitas@4all.com>
2019-05-02 17:22:49 -0300

    :tada:

---
+++ src/Examples/FunctionalComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MeuComponenteFuncional (props) {
+
+  return (
+    <div>
+      <h1>Olá eu sou um componente funcional</h1>
+      <p>Estas tags NÃO SÃO TAGS! #descubra</p>
+    </div>
+  )
+}
+
+export default MeuComponenteFuncional;
---
+++ stories/example.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import MeuComponenteClassudo from '../src/Examples/MeuComponenteClassudo';
+import { storiesOf } from '@storybook/react';
+
+storiesOf('Exemplo :D', module)
+  .add('Componente comum', () => <MeuComponenteClassudo />);
