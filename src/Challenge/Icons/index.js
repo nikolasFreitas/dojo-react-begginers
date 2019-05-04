5bcce075c800be2d417d6652b0a0eda299c8bb9c
Nikolas Freitas <nikolas.freitas@4all.com>
2019-05-03 21:19:01 -0300

    :sparkles: Create icons

---
+++ src/Challenge/Icons/index.js
@@ -0,0 +1,5 @@
+export { default as boy2 } from './boy-2.svg';
+export { default as boy } from './boy.svg';
+export { default as man2 } from './man-2.svg';
+export { default as man } from './man.svg';
+export { default as blondMan } from './blond-man.svg';
